8f851145a075f7ae4cef468e5598e5d23fb54345
Samuel STPL <samuelstpierrelariviere@gmail.com>
2020-11-20 10:39:27 -0500

    Cleaned app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,9 @@
 import React, { useState, useEffect } from "react";
 
-
 import { GlobalStyle } from "../Global/GlobalStyles";
 
-
 function App() {
-  const [bacon, setBacon] = useState(null);
-
-  useEffect(() => {
-    fetch("/bacon")
-      .then((res) => res.json())
-      .then((data) => setBacon(data));
-  }, []);
-
-  // return <div>{bacon ? bacon : `...where's my stuff?...`}</div>;
+  useEffect(() => {}, []);
 
   return (
     <div>
