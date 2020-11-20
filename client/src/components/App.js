3297db6be1c512b03083bfa84bd59754bf57c269
Bradley O'Leary <bradley_oleary@outlook.com>
2020-11-20 13:49:07 -0500

    Commit Test

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import { Header } from "./Header";
 import { GlobalStyle } from "../Global/GlobalStyles";
 import ItemDetails from "./ItemDetails";
-import PurchaseModal from "./PurchaseModal/PurchaseModal";
 
 function App() {
   return (
     <div>
       {/* Css Reset + general font-style */}
       <GlobalStyle />
-      <ItemDetails />
       <Header />
       <PurchaseModal />
     </div>
