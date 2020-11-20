50b09606b4cd2eaad441db4979e954ff21da616d
Samuel STPL <samuelstpierrelariviere@gmail.com>
2020-11-20 16:13:12 -0500

    Combined sided bar plumbing and initial design

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,22 +1,66 @@
 import React from "react";
 import styled from "styled-components";
+import { Sidebar } from "./Sidebar";
 
-export const Homepage = () => {
+export const Homepage = ({ category, setCategory }) => {
   return (
     <HomePageWrapper>
       <SidebarWrapper>
-        {/* TODO Add Sidebar component  */}Menu here
+        <Sidebar setCategory={setCategory} />
       </SidebarWrapper>
       <ItemDisplayGridWrapper>
-        All the items here!
-        {/* TODO Add ItemGrid component  */}
+        {category === "all" &&
+          // TODO Add All Items Components
+          "All Items "}
+
+        {category === "entertainement" &&
+          // TODO Add entertainement Components
+          "entertainement Components "}
+
+        {category === "fitness" &&
+          // TODO Add Fitness Components
+          "Fitness Components "}
+
+        {category === "gaming" &&
+          // TODO Add gaming Components
+          "gaming Components "}
+
+        {category === "industrial" &&
+          // TODO Add industrial Components
+          "industrial Components "}
+
+        {category === "lifestyle" &&
+          // TODO Add lifestyle Components
+          "lifestyle Components "}
+
+        {category === "medical" &&
+          // TODO Add medical Components
+          "medical Components "}
+
+        {category === "pets&animals" &&
+          // TODO Add pets&animals Components
+          "pets&animals Components "}
       </ItemDisplayGridWrapper>
       <CartWrapper>{/* TODO Add Cart component  */}Cart Here</CartWrapper>
     </HomePageWrapper>
   );
 };
 
-const HomePageWrapper = styled.div``;
-const SidebarWrapper = styled.div``;
-const ItemDisplayGridWrapper = styled.div``;
-const CartWrapper = styled.div``;
+const HomePageWrapper = styled.div`
+  position: relative;
+  display: grid;
+  grid-template-columns: (1fr, 2fr, 1fr);
+  grid-template-areas: "sidebar main main cart";
+`;
+
+const SidebarWrapper = styled.div`
+  grid-area: sidebar;
+`;
+
+const ItemDisplayGridWrapper = styled.div`
+  grid-area: main;
+`;
+
+const CartWrapper = styled.div`
+  grid-area: cart;
+`;
