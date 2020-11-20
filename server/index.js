3100a5b531dc030cd3b09088df533a0079f4f3bc
Bradley O'Leary <bradley_oleary@outlook.com>
2020-11-20 10:10:12 -0500

    Test

--- server/index.js
+++ server/index.js
@@ -1,30 +1,75 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 const PORT = 4000;
 
 express()
-  .use(function(req, res, next) {
+  .use(function (req, res, next) {
     res.header(
-      'Access-Control-Allow-Methods',
-      'OPTIONS, HEAD, GET, PUT, POST, DELETE'
+      "Access-Control-Allow-Methods",
+      "OPTIONS, HEAD, GET, PUT, POST, DELETE"
     );
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   })
-  .use(morgan('tiny'))
-  .use(express.static('./server/assets'))
+  .use(morgan("tiny"))
+  .use(express.static("./server/assets"))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
-  .use('/', express.static(__dirname + '/'))
+  .use("/", express.static(__dirname + "/"))
 
   // REST endpoints?
-  .get('/bacon', (req, res) => res.status(200).json('🥓'))
+  .get("/bacon", (req, res) => res.status(200).json("🥓"))
+
+  // Endpoints that retrieves all items
+  .get("/items", (req, res) => {
+    // CODE HERE
+    res.status(200).json({
+      status: 200,
+      message: "Successfully retrieved all items",
+    });
+  })
+
+  // Endpoints that retrieves a specific
+  .get("/items/:itemId", (req, res) => {
+    // CODE HERE
+    res.status(200).json({
+      status: 200,
+      message: `Successfully retrieved item ${itemId} `,
+    });
+  })
+
+  // Endpoints that updates the quantity about specific item
+  .post("/items/:itemId", (req, res) => {
+    // CODE HERE
+    res.status(200).json({
+      status: 200,
+      message: `Successfully updated quantity of item ${itemId} `,
+    });
+  })
+
+  // Endpoints that retrieves a list of all companies
+  .get("/companies", (req, res) => {
+    // CODE HERE
+    res.status(200).json({
+      status: 200,
+      message: `Successfully retrieved all companies`,
+    });
+  })
+
+  // Endpoints that retrieves a list of all companies
+  .get("/companies/:companyId", (req, res) => {
+    // CODE HERE
+    res.status(200).json({
+      status: 200,
+      message: `Successfully retrieved ${companyId}'s info`,
+    });
+  })
 
   .listen(PORT, () => console.info(`Listening on port ${PORT}`));
